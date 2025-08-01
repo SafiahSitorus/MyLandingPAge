23a1211271dda00b44b78fd1e347029582cfb813
safiah.sitorus <safiah.sitorus@sprintasia.co.id>
2025-08-01 10:56:49 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,43 +2,51 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+// import Navbar from "./components/Navbar"; // opsional
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
-function App() {
-  const particlesInit = async (main) => {
-    await loadFull(main);
-  };
+const particlesInit = async (main) => {
+  await loadFull(main);
+};
 
+<Particles
+  id="tsparticles"
+  init={particlesInit}
+  options={{
+    background: { color: "#000000" },
+    particles: {
+      color: { value: "#ffffff" },
+      links: {
+        enable: true,
+        color: "#ffffff",
+        distance: 150,
+        opacity: 0.4,
+        width: 1
+      },
+      move: { enable: true, speed: 1 },
+      number: { value: 50 },
+      opacity: { value: 0.5 },
+      shape: { type: "circle" },
+      size: { value: { min: 1, max: 3 } },
+    },
+    fullScreen: { zIndex: -1 }
+  }}
+/>
+
+function App() {
   return (
     <Router basename="/MyLandingPAge">
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        options={{
-          background: { color: "#000000" },
-          particles: {
-            color: { value: "#ffffff" },
-            links: {
-              enable: true,
-              color: "#ffffff",
-              distance: 150,
-              opacity: 0.4,
-              width: 1
-            },
-            move: { enable: true, speed: 1 },
-            number: { value: 50 },
-            opacity: { value: 0.5 },
-            shape: { type: "circle" },
-            size: { value: { min: 1, max: 3 } },
-          },
-          fullScreen: { zIndex: -1 }
-        }}
-      />
+      {/* <Navbar /> */}
+      
+      <Particles />
+   <AppRoutes />
 
-      <AppRoutes />
+   
     </Router>
   );
 }
+
+
 
 export default App;
