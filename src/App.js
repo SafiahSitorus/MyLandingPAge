ed78d2bd8bc240d9346cd20922c27d5d4597cfa9
safiah.sitorus <safiah.sitorus@sprintasia.co.id>
2025-08-01 10:54:25 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -2,51 +2,43 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-// import Navbar from "./components/Navbar"; // opsional
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
-const particlesInit = async (main) => {
-  await loadFull(main);
-};
-
-<Particles
-  id="tsparticles"
-  init={particlesInit}
-  options={{
-    background: { color: "#000000" },
-    particles: {
-      color: { value: "#ffffff" },
-      links: {
-        enable: true,
-        color: "#ffffff",
-        distance: 150,
-        opacity: 0.4,
-        width: 1
-      },
-      move: { enable: true, speed: 1 },
-      number: { value: 50 },
-      opacity: { value: 0.5 },
-      shape: { type: "circle" },
-      size: { value: { min: 1, max: 3 } },
-    },
-    fullScreen: { zIndex: -1 }
-  }}
-/>
-
 function App() {
-  return (
-    <Router>
-      {/* <Navbar /> */}
-      
-      <Particles />
-   <AppRoutes />
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
 
-   
+  return (
+    <Router basename="/MyLandingPAge">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          background: { color: "#000000" },
+          particles: {
+            color: { value: "#ffffff" },
+            links: {
+              enable: true,
+              color: "#ffffff",
+              distance: 150,
+              opacity: 0.4,
+              width: 1
+            },
+            move: { enable: true, speed: 1 },
+            number: { value: 50 },
+            opacity: { value: 0.5 },
+            shape: { type: "circle" },
+            size: { value: { min: 1, max: 3 } },
+          },
+          fullScreen: { zIndex: -1 }
+        }}
+      />
+
+      <AppRoutes />
     </Router>
   );
 }
-
-
 
 export default App;
